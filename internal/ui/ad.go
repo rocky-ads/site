fbd3a9863308a0ee3937c668cbabcb238b08543e
Scott Feldman <sfeldma@gmail.com>
2026-06-14 21:42:37 -0700

    ad listing age to ad detail regardless of age

--- internal/ui/ad.go
+++ internal/ui/ad.go
@@ -52,10 +52,24 @@ func gridImageNode(adID, count, current int) g.Node {
 
 func formatListedAge(t time.Time) string {
 	d := time.Since(t)
-	days := int(d.Hours() / 24)
-	if days == 0 {
-		return "Listed today"
+	if d < time.Minute {
+		return "Listed just now"
 	}
+	if d < time.Hour {
+		mins := int(d.Minutes())
+		if mins == 1 {
+			return "Listed 1 minute ago"
+		}
+		return fmt.Sprintf("Listed %d minutes ago", mins)
+	}
+	if d < 24*time.Hour {
+		hours := int(d.Hours())
+		if hours == 1 {
+			return "Listed 1 hour ago"
+		}
+		return fmt.Sprintf("Listed %d hours ago", hours)
+	}
+	days := int(d.Hours() / 24)
 	if days == 1 {
 		return "Listed 1 day ago"
 	}
@@ -98,9 +112,6 @@ func newBadgeImageOverlay() g.Node {
 }
 
 func listedAgeDetailNode(createdAt time.Time) g.Node {
-	if time.Since(createdAt) < 4*time.Hour {
-		return g.Text("")
-	}
 	return Div(
 		Class("shrink-0 text-right"),
 		g.Text(formatListedAge(createdAt)),
