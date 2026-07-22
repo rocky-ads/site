cd391498e3dd1fb835d269a51b993243667821f8
Scott Feldman <sfeldma@gmail.com>
2026-07-21 19:45:25 -0700

    add negative margin for mobile grid view

--- internal/ui/home.go
+++ internal/ui/home.go
@@ -268,7 +268,7 @@ func searchResults(view int, results []g.Node, oob bool) g.Node {
 
 	switch view {
 	case ViewGrid:
-		class = "grid grid-cols-2 md:grid-cols-3 gap-2 -mx-5"
+		class = "grid grid-cols-2 md:grid-cols-3 gap-2 -mx-5 md:mx-0"
 	case ViewList:
 		// Empty class - items naturally stack as a column
 	}
